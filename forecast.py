508e34568ee5dde50cfc38c011e9e9ff9c290890
juliaguerra22 <112509909+juliaguerra22@users.noreply.github.com>
2025-12-02 21:28:39 -0500

    forecast.py

---
+++ forecast.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
